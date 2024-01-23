93ff8cab194193123ffab8c61c7e20742922f1f8
Kaviyapriya-12 <kaviyapriya1204@gmail.com>
2024-01-23 15:55:52 +0530

    version1

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Hello ()
+{
+    return(
+        <div>
+            <p>This is a paragraph</p>
+            <h2 align="center">A powerful online engagement tool that’s intuitive and simple to use.</h2>
+    <h4 align="center" >With stellar one-click reports and unmatched support, see how Circle will make a difference in your business.</h4>
+    <h3 >Get Started Free  </h3>
+        </div>
+    )
+}
